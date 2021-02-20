ee3fcffd838c87adf018d4784b67915f873489e7
Doyin Olarewaju <doyinolarewaju@gmail.com>
2021-02-20 15:14:25 +0100

    Add Type annotation to nested field

--- functions/src/models/company-dto.ts
+++ functions/src/models/company-dto.ts
@@ -1,8 +1,11 @@
 import {UserReferenceEntity} from "./reference-entity-types";
+import {Type} from "class-transformer";
 
 class CompanyDTO {
     id?:string;
     name = '';
+
+    @Type (() => UserReferenceEntity)
     users:Array<UserReferenceEntity> = [];
 }
 
